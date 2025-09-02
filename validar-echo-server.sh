0af7db70189d53458414d5ee35e032acc697cc0d
menda <jmendana@fi.uba.ar>
2025-09-01 23:29:55 -0300

    removed extra brackets

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -6,7 +6,7 @@ MESSAGE="Hi server"
 
 RESPONSE=$(docker run --rm --network="$NETWORK_NAME" alpine /bin/sh -c "echo '$MESSAGE' | nc $SERVER_CONTAINER_NAME $SERVER_PORT") 
 
-if [[ "$RESPONSE" == "$MESSAGE" ]]; then
+if [ "$RESPONSE" == "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
     exit 0
 else
