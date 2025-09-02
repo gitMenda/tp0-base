bfd4a60ebff1137469d5cfd0a2aed22e38f32ae7
menda <jmendana@fi.uba.ar>
2025-09-01 23:36:27 -0300

    changed == to =

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,12 +1,11 @@
 NETWORK_NAME="tp0_testing_net"
 SERVER_CONTAINER_NAME="server"
-SERVER_IMAGE="server:latest"
 SERVER_PORT="12345"
 MESSAGE="Hi server"
 
 RESPONSE=$(docker run --rm --network="$NETWORK_NAME" alpine /bin/sh -c "echo '$MESSAGE' | nc $SERVER_CONTAINER_NAME $SERVER_PORT") 
 
-if [ "$RESPONSE" == "$MESSAGE" ]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
     exit 0
 else
