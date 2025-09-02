9d8308ac2a60702254a6cbf7dad46e6c32945ee5
menda <jmendana@fi.uba.ar>
2025-09-01 23:25:48 -0300

    script now assumes server container and network are already created

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,34 +1,10 @@
-NETWORK_NAME="echo_test_net"
-SERVER_CONTAINER_NAME="test_server"
+NETWORK_NAME="tp0_testing_net"
+SERVER_CONTAINER_NAME="server"
 SERVER_IMAGE="server:latest"
-TEST_CLIENT_IMAGE="busybox"
 SERVER_PORT="12345"
 MESSAGE="Hi server"
 
-# function to stop de containers and remove the network
-function cleanup {
-    docker stop "$SERVER_CONTAINER_NAME" >/dev/null 2>&1
-    docker network rm "$NETWORK_NAME" >/dev/null 2>&1
-}
-trap cleanup EXIT
-
-# creation of the docker network
-if ! docker network create "$NETWORK_NAME" >/dev/null 2>&1; then
-    true
-fi
-
-# we now run the server container
-if ! docker run -d --rm --name "$SERVER_CONTAINER_NAME" --network "$NETWORK_NAME" --entrypoint python3 "$SERVER_IMAGE" /main.py >/dev/null 2>&1; then
-    echo "action: test_echo_server | result: fail"
-    exit 1
-fi
-
-# todo: remove?
-sleep 2
-
-# we then run the netcat container to act as the client that communicates with the server
-# we use --rm to make it a temporary container, meaning it will be removed after it exits
-RESPONSE=$(docker run --rm --network "$NETWORK_NAME" "$TEST_CLIENT_IMAGE" sh -c "echo -n \"$MESSAGE\" | nc -w 5 $SERVER_CONTAINER_NAME $SERVER_PORT")
+RESPONSE=$(docker run --rm --network="$NETWORK_NAME" alpine /bin/sh -c "echo '$MESSAGE' | nc $SERVER_CONTAINER_NAME $SERVER_PORT") 
 
 if [[ "$RESPONSE" == "$MESSAGE" ]]; then
     echo "action: test_echo_server | result: success"
